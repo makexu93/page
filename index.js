29a1e370e0ae7768836a5c2b799c895d1239cdfa
Marco Xu <marcoxu@businessdatascience.de>
2020-12-24 11:27:55 +0100

    add scroll to top button

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+
+
+////scroll to top button
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -14,14 +16,20 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  window.scrollTo({top: 0, behavior: 'smooth'});
+
 };
+
+
+
+
+
 
 // Progress bar
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
   myFunction()
+  scrollFunction()
 };
 
 function myFunction() {
@@ -32,6 +40,9 @@ function myFunction() {
 };
 
 
+
+
+//smooth scrolling across entire website
 $(document).ready(function() {
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
